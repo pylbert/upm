8256866bca39914fc4b040f2881f374dc240bf3d
Noel Eck <noel.eck@intel.com>
2018-04-18 14:21:41 -0700

    kx122: Updates for example
        * Check return of getSamplePeriod since -1.0 is a failing case
        * Removed pointer usage from example
        * Updated to use vector method
        * Replace C with C++
    
    Signed-off-by: Noel Eck <noel.eck@intel.com>

--- examples/c++/kx122.cxx
+++ examples/c++/kx122.cxx
@@ -1,65 +1,74 @@
 /*
-* The MIT License (MIT)
-*
-* Author: Samuli Rissanen <samuli.rissanen@hotmail.com>
-* Copyright (c) 2018 Rohm Semiconductor.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy of
-* this software and associated documentation files (the "Software"), to deal in
-* the Software without restriction, including without limitation the rights to
-* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-* the Software, and to permit persons to whom the Software is furnished to do so,
-* subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * The MIT License (MIT)
+ *
+ * Author: Samuli Rissanen <samuli.rissanen@hotmail.com>
+ * Copyright (c) 2018 Rohm Semiconductor.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #include <iostream>
-
 #include <signal.h>
 #include <unistd.h>
 
 #include "kx122.hpp"
 #include "upm_utilities.h"
 
-bool shouldRun = true;
+bool ctrl_c = false;
 
 void sig_handler(int signo)
 {
-  if (signo == SIGINT){
-    shouldRun = false;
-  }
+    ctrl_c = signo == SIGINT;
 }
 
-int main(int argc, char **argv)
+int main()
 {
-  signal(SIGINT,sig_handler);
+    signal(SIGINT, sig_handler);
 
-  //! [Interesting]
-  upm::KX122 sensor(0,-1,24);
+    //! [Interesting]
+    upm::KX122 sensor(0, -1, 24);
 
-  sensor.softwareReset();
-  sensor.deviceInit(KX122_ODR_50,HIGH_RES,KX122_RANGE_2G);
+    /* Initiate blocking device reset */
+    sensor.softwareReset();
 
-  float x,y,z;
-  int wait_time = sensor.getSamplePeriod() * MICRO_S;
-  if (wait_time < 0) wait_time = 1000;
+    /* Initialize to 50 Hz, high resolution mode, 2G */
+    sensor.deviceInit(KX122_ODR_50, HIGH_RES, KX122_RANGE_2G);
 
-  while(shouldRun){
-    sensor.getAccelerationData(&x,&y,&z);
+    /* Get the sample period in microseconds (should be 1/50 Hz = 20000 us) */
+    int T_us = sensor.getSamplePeriod() * MICRO_S;
+    if (T_us < 0)
+    {
+        std::cerr << "Failed to read valid sample period" << std::endl;
+        return -1;
+    }
 
-    printf("%.02f | %.02f | %.02f\n",x,y,z);
-    upm_delay_us(wait_time);
-  }
-  //! [Interesting]
+    std::cout << "Sampling period: " << T_us << " us" << std::endl;
 
-  return 0;
+    while (!ctrl_c) {
+        std::vector<float> xyz = sensor.getAccelerationDataVector();
+        std::cout << "Acceleration X: " << xyz[0] << " m/s²" << std::endl
+                  << "Acceleration Y: " << xyz[1] << " m/s²" << std::endl
+                  << "Acceleration Z: " << xyz[2] << " m/s²" << std::endl
+                  << std::endl;
+
+        upm_delay_us(T_us);
+    }
+    //! [Interesting]
+
+    return 0;
 }
